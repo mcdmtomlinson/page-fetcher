055b2c212673fc5965fd2a816f7829463b73c6c5
Monica Tomlinson <mcdmtomlinson@outlook.com>
2024-04-11 18:02:05 -0600

    start activity

--- fetcher.js
+++ fetcher.js
@@ -1,27 +1,5 @@
-const fs = require('fs'); //import file system
-const request = require('request'); // import request module
+const fs = require('fs');
+const request = require('request');
 
-const url = process.argv[2]; //url at [2] in command line
-const localFile = process.argv[3]; //local file at [3] in command line
-
-const fileStream = fs.createWriteStream(localfile); /*create a writeable stream to a local file */
-
-request(url)
-  .on('response', response) => { //upon a response
-    if (response.statusCode !== 200){ //if anything but code 200
-    console.error('Error: Received status code ${response.statusCode}');
-    fileStream.close();
-  return;
-}
-response.on('data', data => { //when 'data is triggered
-  fileStream.write(data); //write the data to filestream
-});
-
-response.on('end', () => { // upon .end event in data
-  fileStream.close(); // close the fileStream
-  const fileSize = fs.statSync(localfile).size; /* Get the size of the downloaded file */
-  console.log(`Downloaded and saved ${fileSize} bytes to ${localFile}`);
-}); //log the file size that went into localFile
-  };
-
-  
+const url = process.argv.slice(2)[0];
+const target = process.argv.slice(2)[1];
